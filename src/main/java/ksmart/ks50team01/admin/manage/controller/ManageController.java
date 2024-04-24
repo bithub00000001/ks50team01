e17b60981652fd17d3842baa1590e67129f9e5a1
dare <ASUS@DESKTOP-T50VO8O>
2024-04-24 09:22:01 +0900

    [TEAM-박성민] 템플림 카테고리 동작 확인
    detail: 동작확인 완료

--- src/main/java/ksmart/ks50team01/admin/manage/controller/ManageController.java
+++ src/main/java/ksmart/ks50team01/admin/manage/controller/ManageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/manage")
+@RequestMapping("/admin/manage")
 public class ManageController {
 	
 	@GetMapping("/managecate")
