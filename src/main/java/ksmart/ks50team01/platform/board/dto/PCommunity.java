3710216af13f70bbb999c6b12b93d7a4c0cc663f
ASUS <ASUS@DESKTOP-T50VO8O>
2024-04-25 17:33:58 +0900

    [ks50team01] 공지사항

--- src/main/java/ksmart/ks50team01/platform/board/dto/PCommunity.java
+++ src/main/java/ksmart/ks50team01/platform/board/dto/PCommunity.java
@@ -1,5 +1,5 @@
 package ksmart.ks50team01.platform.board.dto;
 
-public class PQnaDto {
+public class PCommunity {
 
 }
---
+++ src/main/java/ksmart/ks50team01/user/board/dto/UCommunity.java
@@ -0,0 +1,5 @@
+package ksmart.ks50team01.user.board.dto;
+
+public class UCommunity {
+
+}
