99b8f33cfd09dec6d94d9ef64f40f6319baa8baf
gethub <1224.yuhee@gmail.com>
2024-04-30 17:17:41 +0900

    [ks50tema01 - yh] 검색버튼 추가

---
+++ src/main/java/ksmart/ks50team01/platform/reivew/controller/PContentController.java
@@ -0,0 +1,25 @@
+package ksmart.ks50team01.platform.reivew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping(value="/platform")
+@RequiredArgsConstructor
+public class PContentController {
+	
+	@GetMapping("/review")
+	public String getMethodName(@RequestParam String param) {
+		
+		
+		
+		return "platform/member/memberManagement";
+	}
+	
+
+}
