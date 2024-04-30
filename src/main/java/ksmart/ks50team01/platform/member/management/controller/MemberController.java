0bd13eca2cc8aadcfcff5ab4871dda64ee8bbd81
ASUS <ASUS@DESKTOP-T50VO8O>
2024-04-30 16:59:02 +0900

    [ks50team01-khm] 회원정보관리 수정 detail : 2차수정

--- src/main/java/ksmart/ks50team01/platform/member/management/controller/MemberController.java
+++ src/main/java/ksmart/ks50team01/platform/member/management/controller/MemberController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ksmart.ks50team01.platform.member.management.dto.Member;
 import ksmart.ks50team01.platform.member.management.service.MemberService;
@@ -17,6 +19,29 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 
 	public final MemberService memberService;
+	
+	@GetMapping("/memberGrade")
+	public String getMemberGrade(Model model) {
+		
+		return "platform/member/memberGrade";
+	}
+	
+	
+	@PostMapping("/memberGrade2")
+	public String modifyMember(@RequestParam(value="memberId") String memberId
+							  ,Model model) {
+
+		Member memberInfo = memberService.getMemberInfoById(memberId);
+		List<Member> memberList = memberService.getMemberList();
+		
+		model.addAttribute("title", "회원수정");
+		model.addAttribute("memberInfo", memberInfo);
+		model.addAttribute("memberList", memberList);
+		
+
+		
+		return "platform/member/memberGrade2";
+	}
 	
 	
 	@GetMapping("/memberManagement")
