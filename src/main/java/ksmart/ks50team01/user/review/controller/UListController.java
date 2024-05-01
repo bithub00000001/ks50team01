07a69db289108b93a3939155f139d1194665fae3
USER <USER@192.168.219.102>
2024-05-01 22:52:14 +0900

    [ks50team01 - yh] 신고하기 모달창, 리뷰 리스트 페이지(미완)

---
+++ src/main/java/ksmart/ks50team01/user/review/controller/UListController.java
@@ -0,0 +1,21 @@
+package ksmart.ks50team01.user.review.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping(value="/review")
+@RequiredArgsConstructor
+public class UListController {
+	
+	@GetMapping("/list")
+	public String reviewList() {
+		return "user/review/reviewList";
+	}
+	
+}
