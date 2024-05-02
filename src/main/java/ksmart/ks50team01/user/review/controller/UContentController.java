61d42d2e4ad4b13ebb22b4f36780abe9161e3c31
gethub <1224.yuhee@gmail.com>
2024-05-02 16:12:04 +0900

    [ks50team - yh] 저장용

--- src/main/java/ksmart/ks50team01/user/review/controller/UContentController.java
+++ src/main/java/ksmart/ks50team01/user/review/controller/UContentController.java
@@ -3,13 +3,11 @@ package ksmart.ks50team01.user.review.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 @RequestMapping(value="/user/review")
-@RequiredArgsConstructor
 public class UContentController {
 	
 	@GetMapping("/write")
--- src/main/java/ksmart/ks50team01/user/review/controller/UReportController.java
+++ src/main/java/ksmart/ks50team01/user/review/controller/UReportController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping(value="/user/review")
-@RequiredArgsConstructor
 public class UReportController {
 	
 	@GetMapping("/report")
