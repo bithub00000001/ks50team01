c5fd08307c5a08bcb61fa3ae1c2c6a62aaf347e1
ASUS <ASUS@DESKTOP-T50VO8O>
2024-05-02 15:34:50 +0900

    [ks50team01] faq qna community report notice detail : 화면

--- src/main/java/ksmart/ks50team01/user/board/controller/UQnaController.java
+++ src/main/java/ksmart/ks50team01/user/board/controller/UQnaController.java
@@ -6,14 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/board")
+@RequestMapping(value = "user/board")
 public class UQnaController {
 	
-	@GetMapping("/qna")
+	@GetMapping("/qnaList")
 	public String qna(Model model) {
 		
 		model.addAttribute("title", "1:1문의내역페이지");
 		return "user/board/qnaList";
+		
+	}
+	
+	@GetMapping("/qnaWrite")
+	public String qnaWrite(Model model) {
+		
+		model.addAttribute("title", "1:1문의글쓰기");
+		return "user/board/qnaWrite";
 		
 	}
 
--- src/main/java/ksmart/ks50team01/user/board/controller/UReportController.java
+++ src/main/java/ksmart/ks50team01/user/board/controller/UReportController.java
@@ -1,5 +1,19 @@
 package ksmart.ks50team01.user.board.controller;
 
-public class UReportController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
+@RequestMapping(value = "/user/board")
+public class UReportController {
+	
+	@GetMapping("/report")
+	public String report(Model model) {
+		
+		return "user/board/report";
+	}
+	
 }
