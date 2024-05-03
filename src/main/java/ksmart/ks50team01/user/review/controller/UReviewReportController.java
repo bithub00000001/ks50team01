c7908fd133660d1b88895ae01459046955fca5a2
gethub <1224.yuhee@gmail.com>
2024-05-03 16:22:32 +0900

    [ks50team - yh] 모달창 합침 detail: 기존 페이지를 모달로 만들었으니 나중에 report css, controller, html 삭제하기 사업자 답글도 비지니스쪽으로 옮길때 삭제할 것

--- src/main/java/ksmart/ks50team01/user/review/controller/UReviewReportController.java
+++ src/main/java/ksmart/ks50team01/user/review/controller/UReviewReportController.java
@@ -1,18 +1,18 @@
 package ksmart.ks50team01.user.review.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping(value="/user/review")
-public class UListController {
+public class UReviewReportController {
 	
-	@GetMapping("/list")
-	public String reviewList() {
-		return "user/review/reviewList";
+	@GetMapping("/report")
+	public String reviewWrite() {
+		return "user/review/reviewReport";
 	}
 	
+
 }
