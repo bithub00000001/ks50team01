fe7f26777c85f63a18ddb7bf269c5144114c2f7d
82108 <82108@192.168.219.105>
2024-05-07 20:28:13 +0900

    [ks50team1-khm]user 로그인,로그아웃 기능 추가

---
+++ src/main/java/ksmart/ks50team01/user/member/login/dto/Login.java
@@ -0,0 +1,13 @@
+package ksmart.ks50team01.user.member.login.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Login {
+
+	private String id;
+	private String password;
+	private String name;
+}
--- src/main/java/ksmart/ks50team01/user/member/login/dto/LoginResponse.java
+++
@@ -1,5 +0,0 @@
-package ksmart.ks50team01.user.member.login.dto;
-
-public class LoginResponse {
-
-}
