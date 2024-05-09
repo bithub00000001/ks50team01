62a496d2f2a3df55ba4e4738cb64e38bbbcfa0af
ASUS <ASUS@DESKTOP-T50VO8O>
2024-05-09 17:21:25 +0900

    [ks50team01] 관리자 페이지 detail : 수정

--- src/main/java/ksmart/ks50team01/platform/board/service/PCommunityService.java
+++ src/main/java/ksmart/ks50team01/platform/board/service/PCommunityService.java
@@ -1,5 +1,39 @@
 package ksmart.ks50team01.platform.board.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ksmart.ks50team01.platform.board.dto.PCommunity;
+import ksmart.ks50team01.platform.board.mapper.PCommunityMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
 public class PCommunityService {
+	
+	private PCommunityMapper pCommunityMapper;
+	
+	/**
+	 * 게시글 조회
+	 * @return List<PCommunity>
+	 */
+	public List<PCommunity> getPostList(){
+		return pCommunityMapper.getPostList();
+	}
+	
+	
+	/**
+	 * 댓글 조회
+	 * @return List<PCommunity>
+	 */
+	public List<PCommunity> getCommentList(){
+		return pCommunityMapper.getCommentList();
+	}
+	
 
 }
