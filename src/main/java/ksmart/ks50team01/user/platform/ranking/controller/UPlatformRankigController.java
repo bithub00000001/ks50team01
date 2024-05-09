439150eb11585dd52e7a838ea3bfb08d2d4e1707
ECS <ECS@DESKTOP-048HU3O>
2024-05-09 14:26:37 +0900

    [ks50team01 - scw] 추천페이지 수정 detail: breadcrumb 및 파일네이밍 등 수정

--- src/main/java/ksmart/ks50team01/user/platform/ranking/controller/UPlatformRankigController.java
+++ src/main/java/ksmart/ks50team01/user/platform/ranking/controller/UPlatformRankigController.java
@@ -8,30 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequestMapping(value="/user")
+@RequestMapping(value="/allRanking")
 @RequiredArgsConstructor
 public class UPlatformRankigController {
 
-	@GetMapping("/ranking")
+	@GetMapping
 	public String platformRanking(Model model) {
 		
-		model.addAttribute("title", "추천페이지");
-		return "user/platformranking/ranking";
+		model.addAttribute("title", "여행 추천");
+		return "user/platformranking/allRanking";
 	}
 	
-	@GetMapping("/ranking/rankingList")
+	@GetMapping("/rankingList")
 	public String platformRankingList(Model model) {
 		
-		model.addAttribute("title", "플랫폼 추천 인기순위");
+		model.addAttribute("title", "플랫폼 추천 순위");
 		return "user/platformranking/rankingList";
 	}
-	@GetMapping("/ranking/userRankingList")
+	@GetMapping("/userRankingList")
 	public String userRankingList(Model model) {
 		
 		model.addAttribute("title", "회원추천 인기순위");
 		return "user/platformranking/userRankingList";
 	}
-	@GetMapping("/ranking/planRankingList")
+	@GetMapping("/planRankingList")
 	public String planRankingList(Model model) {
 		
 		model.addAttribute("title", "다른회원의 여행계획 추천");
