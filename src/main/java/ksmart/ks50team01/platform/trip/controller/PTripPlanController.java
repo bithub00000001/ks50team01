bc1f9e172f15c2c314713638063eab6e120a564e
bitHub <bitto7202@gmail.com>
2024-05-10 17:37:49 +0900

    [ks50team01-bit] Create plan modification page, not yet complete detail: 240510 Edit:해야할 일 - PTripPlanMapper에 주석으로 정리함

--- src/main/java/ksmart/ks50team01/platform/trip/controller/PTripPlanController.java
+++ src/main/java/ksmart/ks50team01/platform/trip/controller/PTripPlanController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ksmart.ks50team01.platform.trip.dto.PTripPlan;
 import ksmart.ks50team01.platform.trip.service.PTripPlanService;
@@ -20,6 +22,26 @@ public class PTripPlanController {
 
 	private final PTripPlanService pTripPlanService;
 
+	// 여행 계획 목록 수정 post 요청
+	@PostMapping("/modify")
+	public String modify(PTripPlan pTripPlan, Model model) {
+		model.addAttribute("title", "여행 계획 수정 페이지");
+		return "redirect:/platform/plan/list";
+	}
+
+	// 여행 계획 목록 수정 페이지
+	@GetMapping("/modify")
+	public String planModify(@RequestParam(name = "planId") String planId,
+		Model model) {
+		PTripPlan pTripPlan = pTripPlanService.getPTripPlanById(planId);
+		model.addAttribute("title", "여행 계획 수정");
+		model.addAttribute("planInfo", pTripPlan);
+
+		return "/platform/trip/planModify";
+	}
+
+
+	// 여행 계획 목록 조회 페이지
 	@GetMapping("/list")
 	public String planList(Model model) {
 		List<PTripPlan> tripPlanList = pTripPlanService.getAllPTripPlan();
--- src/main/java/ksmart/ks50team01/platform/trip/service/PTripPlanServiceImpl.java
+++ src/main/java/ksmart/ks50team01/platform/trip/service/PTripPlanServiceImpl.java
@@ -21,7 +21,30 @@ public class PTripPlanServiceImpl implements PTripPlanService {
 	@Override
 	public List<PTripPlan> getAllPTripPlan() {
 		List<PTripPlan> tripPlanList = pTripPlanMapper.getPlanList();
+		tripPlanList.forEach(plan -> {
+			if (!plan.isShare()) {
+				plan.setIsShareString("공유 안함");
+			}else {
+				plan.setIsShareString("공유 중");
+			}
+		});
 		log.info("tripPlanList: {}", tripPlanList);
 		return tripPlanList;
+	}
+
+	@Override
+	public PTripPlan getPTripPlanById(String planId) {
+		PTripPlan pTripPlan = pTripPlanMapper.getTripPlanById(planId);
+		if (!pTripPlan.isShare()){
+			pTripPlan.setIsShareString("공유 안함");
+		}else {
+			pTripPlan.setIsShareString("공유 중");
+		}
+		return pTripPlan;
+	}
+
+	@Override
+	public int UpdatePTripPlan(PTripPlan pTripPlan) {
+		return 0;
 	}
 }
