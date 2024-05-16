0202d649c0829eeb26a2b8292a92e6f2d21e0da1
yun <yun@localhost>
2024-05-16 09:02:52 +0900

    [ks50team01] 유저 페이
    detail : 공지사항 상세화면 수정중

--- src/main/java/ksmart/ks50team01/user/board/mapper/UFaqMapper.java
+++ src/main/java/ksmart/ks50team01/user/board/mapper/UFaqMapper.java
@@ -1,5 +1,16 @@
 package ksmart.ks50team01.user.board.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ksmart.ks50team01.user.board.dto.UFaq;
+
+
+@Mapper
 public interface UFaqMapper {
+	
+	// 자주찾는 질문 조회
+	List<UFaq> getFaqList();
 
 }
--- src/main/java/ksmart/ks50team01/user/board/mapper/UNoticeMapper.java
+++ src/main/java/ksmart/ks50team01/user/board/mapper/UNoticeMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import ksmart.ks50team01.user.board.dto.UCommunity;
 import ksmart.ks50team01.user.board.dto.UNotice;
 
 
@@ -11,9 +12,12 @@ import ksmart.ks50team01.user.board.dto.UNotice;
 public interface UNoticeMapper {
 	
 	// 공지사항 상세목록 조회
-	List<UNotice> getNoticeDetailList();
+	UNotice getNoticeByNoticeNum(String noticeNum);
+	//List<UNotice> getNoticeDetailList();
 
 	// 공지사항 목록조회
 	List<UNotice> getNoticeList();
+	
+	
 
 }
