036f5b58fa650f023fbc7fcc4cb0f871a0f6a931
bitHub <bitto7202@gmail.com>
2024-05-20 19:49:57 +0900

    [ks50team01-bit] 여행지 정보 관리 페이지 수정 DETAIL: 240520 19:48 EDIT: default.html       destinationInfoManage.html       PTourApiMapper.java       PTourApiMapper.xml       PTourApiService.java       PTourApiServiceImpl.java       PTourDetail.java       PTripPlanController.java       PTripPlanMapper.java       PTripPlanMapper.xml       PTripPlanService.java       PTripPlanServiceImpl.java       destinationDetailInfo.html TO DO: 지역 이름과 시군구 이름 나오도록 작업 필요

--- src/main/java/ksmart/ks50team01/platform/trip/service/PTripPlanService.java
+++ src/main/java/ksmart/ks50team01/platform/trip/service/PTripPlanService.java
@@ -3,11 +3,12 @@ package ksmart.ks50team01.platform.trip.service;
 import java.util.List;
 
 import ksmart.ks50team01.platform.trip.dto.PTourApi;
+import ksmart.ks50team01.platform.trip.dto.PTourDetail;
 import ksmart.ks50team01.platform.trip.dto.PTripPlan;
 
 public interface PTripPlanService {
 	// 여행 계획 전체 목록 조회
-	List<PTripPlan> getAllPTripPlan();
+	List<PTripPlan> getPTripPlanList();
 
 	// 여행 계획 id에 해당하는 계획 조회
 	PTripPlan getPTripPlanById(String planId);
@@ -29,6 +30,9 @@ public interface PTripPlanService {
 
 	// 컨텐츠 아이디와 일치하는 여행지 정보 목록 조회
 	List<PTourApi> getDestinationListByContentType(String contentTypeId);
+
+	// 컨텐츠 아이디와 일치하는 여행지 세부 정보 조회
+	PTourDetail getPTourDetailByContentId(String contentId, String contentTypeId);
 
 	// 여행지 세부 정보 조회
 }
