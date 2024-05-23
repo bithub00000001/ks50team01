616e0992b48998c4a1ed710d5e7633078f471fcb
ASUS <ASUS@DESKTOP-T50VO8O>
2024-05-23 14:19:47 +0900

    [ks50team01-khm] 내가쓴글보기, 내가쓴댓글보기기능 추가

--- src/main/java/ksmart/ks50team01/user/member/mypage/mapper/MypageMapper.java
+++ src/main/java/ksmart/ks50team01/user/member/mypage/mapper/MypageMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import ksmart.ks50team01.user.member.mypage.dto.Mypage;
+import ksmart.ks50team01.user.member.mypage.dto.Post;
 
 @Mapper
 public interface MypageMapper {
@@ -20,4 +21,10 @@ public interface MypageMapper {
 	
 	// 회원탈퇴
 	int delMember(String memberId);
+	
+	// 내가 쓴 게시글 보기
+	List<Post> getPostsByMemberId(String memberId);
+	
+	// 내가 쓴 댓글 보기
+	List<Post> getPostsCmtByMemberId(String memberId);
 }
--- src/main/resources/static/user/js/mypage.js
+++ src/main/resources/static/user/js/mypage.js
@@ -16,6 +16,56 @@ function getMemberInfo() {
 	});
 }
 
+function getMemberPost() {
+     $.ajax({
+        url: "/mypage/mypage2",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            var postList = "";
+            if (data.length > 0) {
+                for (var i = 0; i < data.length; i++) {
+                    postList += "<tr>" +
+                                    "<td>" + data[i].pstTtl + "</td>" +
+                                    "<td><a href='http://localhost/community/postDetail?postNum=" + data[i].pstNum + "' class='btn btn-primary btn-sm' target='_blank'>바로가기</a></td>" +
+                                "</tr>";
+                }
+            } else {
+                postList = "<tr><td colspan='2'>작성한 게시글이 없습니다.</td></tr>";
+            }
+            $("#postTableBody").html(postList);
+        },
+        error: function(xhr, status, error) {
+            console.log("Error: " + error);
+        }
+    });
+}
+
+function getMemberCmtPost() {
+    $.ajax({
+        url: "/mypage/postCmt",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            var commentList = "";
+            if (data.length > 0) {
+                for (var i = 0; i < data.length; i++) {
+                    commentList += "<tr>" +
+                                    "<td>" + data[i].pstCmtCn + "</td>" +
+                                    "<td><a href='http://localhost/community/postDetail?postNum=" + data[i].pstNum + "' class='btn btn-primary btn-sm' target='_blank'>바로가기</a></td>" +
+                                "</tr>";
+                }
+            } else {
+                commentList = "<tr><td colspan='2'>작성한 댓글이 없습니다.</td></tr>";
+            }
+            $("#commentTableBody").html(commentList);
+        },
+        error: function(xhr, status, error) {
+            console.log("Error: " + error);
+        }
+    });
+}
+
 function submitForm(form) {
 	$.ajax({
 		url: form.action,
